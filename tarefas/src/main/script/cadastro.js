a8697aa35e84ace7b28a68a20932efae76b3f045
Timeward <sonicx1932@gmail.com>
2023-11-26 20:28:45 -0300

    coloquei mais codigo js nos modal, precisa arrumar

--- tarefas/src/main/script/cadastro.js
+++ tarefas/src/main/script/cadastro.js
@@ -5,22 +5,25 @@ const Iemail = document.querySelector(".email");
 const Isenha = document.querySelector(".senha");
 
 
-function cadastrar(){
-    
+function cadastrar(Inome, Iemail, Isenha){
+
+    console.log(Inome)
+
     fetch("http://localhost:8080/usuario",
-    {
-        headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify({ 
-        nome: Inome.value,
-        email: Iemail.value,
-        senha: Isenha.value})
-    })
-    .then(function (res){console.log(res)})
-    .catch(function (res){console.log(res)})
+        {
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            method: "POST",
+            body: JSON.stringify({
+                nome: Inome.value,
+                email: Iemail.value,
+                senha: Isenha.value})
+        })
+        .then(function (res){console.log(res)})
+        .catch(function (res){console.log(res)})
 
 };
 
@@ -30,8 +33,24 @@ function limpar(){
     Isenha.value = ""
 }
 
-formulario.addEventListener('submit', function (event){
-    event.preventDefault();
-    cadastrar();
-    limpar();
-});
+
+
+// Função a ser executada quando a página é completamente carregada
+function pageLoaded() {
+    console.log("A página foi completamente carregada.");
+
+    const formulario = document.getElementById("cadastroForm") ;
+
+    const Inome = document.getElementById("nome");
+    console.log(Inome)
+    const Iemail = document.getElementById("email");
+    const Isenha = document.getElementById("senha");
+
+    formulario.addEventListener('submit', function (event){
+        event.preventDefault();
+        cadastrar(Inome, Iemail, Isenha);
+        // limpar();
+    });
+}
+// Atribui a função ao evento window.onload
+window.onload = pageLoaded;
--- tarefas/src/main/script/dashboard.js
+++ tarefas/src/main/script/dashboard.js
@@ -1,8 +1,10 @@
-function confirmTask() {
     var taskName = document.getElementById("taskName").value;
     var taskDescription = document.getElementById("taskDescription").value;
+    var taskDateTime = document.getElementById("taskDate").value;
     var taskList = document.getElementById("taskList").value;
+    var listName = document.getElementById("listName").value;
 
+    function confirmTask(taskName, taskDescription, taskDate, taskList) {
     // Perform further actions, e.g., store data, update UI, etc.
     fetch("http://localhost:8080/tarefa",
         {
@@ -13,12 +15,35 @@ function confirmTask() {
             method: "POST",
             body: JSON.stringify({
                 nome: taskName.value,
-                email: .value,
-                senha: Isenha.value})
+                descricao: taskDescription.value,
+                data: taskDateTime.value,
+                horario: taskDateTime.value,
+                listaTarefa: taskList.value
+            //TODO: Definitivamente errado na parte de data e horario.
         })
         .then(function (res){console.log(res)})
         .catch(function (res){console.log(res)})
-    
+    })
     // Close the modal
     closeModal('taskModal');
+
+    function confirmList(listName){
+        fetch("http://localhost:8080/listaTarefa",
+        {
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                nome: listName.value
+            //TODO: Definitivamente errado na parte de data e horario.
+        })
+        .then(function (res){console.log(res)})
+        .catch(function (res){console.log(res)})
+    })
+    // Close the modal
+    closeModal('listModal');
+    }
+
 }
